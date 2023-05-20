e9105c7102543dab613a03469d62d48abee058b0
111 <105259819+Tsubaki111@users.noreply.github.com>
2023-05-20 17:53:56 +0800

    Update Top.v

--- CPU_1/CPU_1.srcs/sources_1/new/Top.v
+++ CPU_1/CPU_1.srcs/sources_1/new/Top.v
@@ -16,16 +16,14 @@ module Top(
     output tx
     );
     
-    wire [2:0] num;//用例编号
-    wire [7:0] in;//8个拨码开关
     wire case_yes;
-    wire input1_yes;//确认第一个输入
-    wire input2_yes;//确认第二个输入
+    wire input_yes;//确认输入
+    wire start;//确认开始
     wire switch_case;
     
     assign case_yes=btn[0];
-    assign input1_yes=btn[1];
-    assign input2_yes=btn[2];
+    assign input_yes=btn[1];
+    assign start=btn[2];
     assign switch_case=btn[3];
     
     wire cpu_clk;
@@ -38,24 +36,24 @@ module Top(
     //data to program_rom or dmemory32
     wire [31:0] upg_dat_o; 
     
-    wire spg_bufg;
     wire start_pg;
+    wire spg_bufg;
     parameter son=3'b100, soff=3'b101;
     reg[2:0] state_uart,nxt_uart;
-
+    
     always @* begin //fsms
-        if(fpga_rst)
-            state_uart <= soff;
-        else
-            state_uart <= nxt_uart;
+    if(fpga_rst)
+        state_uart <= soff;
+    else
+        state_uart <= nxt_uart;
     end
-
+    
     always @* begin //fsm-uart
-        case(state_uart)
-            soff: if(btn[5]) nxt_uart=son;
-            son: if(btn[5]) nxt_uart=soff;
-            default: nxt_uart=state_uart;
-        endcase
+    case(state_uart)
+        soff: if(btn[5]) nxt_uart=son;
+        son: if(btn[5]) nxt_uart=soff;
+        default: nxt_uart=state_uart;
+    endcase
     end
     BUFG U1(.I(start_pg), .O(spg_bufg)); // de-twitter
     // Generate UART Programmer reset signal
@@ -75,8 +73,8 @@ module Top(
     
     
     //input部分
-    reg [3:0] state,next_state;
-    parameter s0=3'b000, s1=3'b001, s2=3'b010, s3=011; //s0等待输入case //s1输入case完成 //s2输入input1 s3 输入input2
+    reg [2:0] state,next_state;
+    parameter s0=3'b000, s1=3'b001, s2=3'b010, s3=3'b011 ,s4=3'b100; //s0等待输入case //s1输入case完成 //s2输入input1 s3 输入input2完成 展示中  //s4开始执行
     reg [2:0] cases;
     reg [7:0] ioread_data_switch;
 
@@ -90,14 +88,16 @@ module Top(
     
     always @* begin //fsm-detail
         case(state)
-            s0:if(case_yes) begin next_state = s1; cases=num; end //输入测试样例
+            s0:if(case_yes) begin next_state = s1; cases={switch[2:0]}; end //输入测试样例
                 else next_state=s0;
-            s1:if(input1_yes) begin next_state=s2; ioread_data_switch=in; end // 输入参数1
+            s1:if(input_yes) begin next_state=s2; ioread_data_switch=switch; end // 输入参数1
                 else begin next_state=s1; ioread_data_switch=ioread_data_switch; end
-            s2:if(input2_yes) begin next_state=s3; ioread_data_switch=in; end // 输入参数2
+            s2:if(input_yes) begin next_state=s3; ioread_data_switch=switch; end // 输入参数2--有些测试场景要考虑一下比如只有一个输入 第二个输入会直接按按钮啥的 这个时候就别搞辣么多啥的
                 else begin next_state=s2; ioread_data_switch=ioread_data_switch; end
-            s3:if(switch_case) next_state=s0;   //测试场景结束 切换 
-                else next_state=s3;
+            s3:if(start) next_state=s4;   //展示一下输入的数（有必要的话） 
+               else next_state=s3;
+            s4:if(switch_case) next_state=s0;   //测试场景结束 切换 
+               else next_state=s4;
             default: next_state=next_state;
         endcase
     end
@@ -110,11 +110,10 @@ module Top(
    wire [1:0] ALUOp;
    wire ALUSrc,I_format,Sftmd,Zero,Branch,nBranch,Jmp,Jal,Jr,RegDST,MemorIOtoReg,RegWrite,MemWrite,MemRead,IORead,IOWrite;
    wire LEDCtrl,switchctrl,segctrl,btnctrl;
-
-    assign ioread_data_switch={input1,input2};
     
-    wire[31:0] Instruction_i,Instruction_o;//要注意rst的什么电平有效 mini好像是低电平effective ?  in code-high
+    wire[31:0] Instruction_i,Instruction_o;//要注意rst的什么电平有效 高电平high-effective
     wire[13:0] rom_adr_o;
+    
     programrom pgr(cpu_clk,rom_adr_o,Instruction_i,upg_rst,upg_clk_o,upg_wen_o,upg_adr_o[13:0],upg_dat_o,upg_done_o);
     IFetc32 iff(Instruction_o, branch_base_addr,  Addr_Result, Read_data_1, Branch, nBranch, Jmp, Jal, Jr, Zero,cpu_clk, fpga_rst,link_addr,rom_adr_o,Instruction_i);
 
@@ -142,7 +141,7 @@ module Top(
     
     //基本测试场景2中 溢出检测也是 比较最高一个bit 存在1在一个寄存器中 体现在LED中！！
     //注意：只能存1！！！比较方便
-    leds l(fpga_rst,fpga_clk,IOWrite,LEDCtrl,Addr_Result[1:0],write_data[7:0],ledout);
+    leds l(fpga_rst,fpga_clk,IOWrite,LEDCtrl,write_data[7:0],ledout);
     ioread ur(fpga_rst,IORead,switchctrl,ioread_data_switch,ior_data);
     segtube st(fpga_clk,fpga_rst,segctrl,segwrite,seg_en,seg_out1,seg_out2);
     
@@ -160,8 +159,8 @@ module Top(
     reg [28:0] count_5s;
     wire temp_2s;
     wire temp_5s;
-    assign temp_2s = ((segctrl&&num==3'b010)||(segctrl&&num==3'b011));
-    assign temp_5s = (segctrl&&num==3'b111);
+    assign temp_2s = ((segctrl&&cases==3'b010)||(segctrl&&cases==3'b011));
+    assign temp_5s = (segctrl&&cases==3'b111);
     always@(posedge fpga_clk) begin //count 2s   用于实现每2-3s 一变 
         if(temp_2s) begin
             if(count_2s == 28'd200000000)
@@ -185,10 +184,14 @@ module Top(
     end   
     
     always@(posedge fpga_clk) begin//使用case和计时器来控制每次输出参数停留2s或5s
-        casex(num)
-            3'b01x: begin if(count_2s == 28'd200000000) segwrite=write_data; end
-            3'b111: begin if(count_5s == 29'd500000000) segwrite=write_data; end
-            default: segwrite=segwrite;
+        casex({state,cases})
+            6'b000_xxx: segwrite={32'h0000_0001}; 
+            6'b001_xxx: segwrite={28'h0000_000,1'b0,cases};//刚输入完测试样例-cases3bit
+            6'b010_xxx: segwrite={24'h000000,ioread_data_switch}; //ioread_data_switch 8bit 第一次输入
+            6'b011_xxx: segwrite={24'h000000,ioread_data_switch};//ioread_data_switch 8bit 第二次输入
+            6'b100_01x: begin if(count_2s == 28'd200000000) segwrite=write_data; end
+            6'b100_111: begin if(count_5s == 29'd500000000) segwrite=write_data; end
+            default: segwrite=write_data;
         endcase
     end   
 
